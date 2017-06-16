cf90eb2251f92d6aa9822c0d07cbdc293c423d81
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-15 15:44:33 -1000

    add calculator logic

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,37 @@
+let Calculator = (function () {
 
+    //ALL variables are private
+
+    let x = 0;
+    let total = 0;
+
+    //All methods are private
+
+    function add(x){
+    validation(x);
+    total += x;
+    }
+
+    function subtract(x){
+    validation(x);
+    total -= x;
+    }
+
+    function multiply(x){
+    validation(x);
+    total = total*x;
+    }
+
+    function divide(x){
+    validation(x);
+    total = total/x;
+    }
+
+    //REVEAL only what you want to be public
+    return {
+      add: add,
+      subtract: subtract,
+      multiply: multiply,
+      divide: divide
+    };
+})();
