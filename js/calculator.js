6c876c58b65d9ba28e66dfd945af666307843732
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-15 16:55:33 -1000

    add ids to html and all functions is calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,37 +1,74 @@
-let Calculator = (function () {
+window.calculator = (function () {
 
-    //ALL variables are private
 
-    let x = 0;
-    let total = 0;
 
-    //All methods are private
+  let memory = 0;
+  let total = 0;
 
-    function add(x){
+  function load(x){
+    validation(x);
+    total = x;
+    return total;
+  }
+
+  function getTotal(){
+    return total;
+  }
+
+  function add(x){
     validation(x);
     total += x;
-    }
+  }
 
-    function subtract(x){
+  function subtract(x){
     validation(x);
     total -= x;
-    }
+  }
 
-    function multiply(x){
+  function multiply(x){
     validation(x);
     total = total*x;
-    }
+  }
 
-    function divide(x){
+  function divide(x){
     validation(x);
     total = total/x;
-    }
+  }
 
-    //REVEAL only what you want to be public
-    return {
-      add: add,
-      subtract: subtract,
-      multiply: multiply,
-      divide: divide
-    };
+
+  function recallMemory(){
+    return memory;
+  }
+
+
+  function saveMemory(){
+    memory = total;
+  }
+
+  function clearMemory(){
+    memory = 0;
+  }
+
+  /**
+   * Validation
+   */
+  function validation(x){
+    if(typeof x !== "number"){
+      throw new Error("Error!!!");
+    }
+  }
+
+  return {
+
+  load: load,
+  getTotal: getTotal,
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+  recallMemory: recallMemory,
+  saveMemory: saveMemory,
+  clearMemory: clearMemory
+  };
+
 })();
---
+++ js/cash_register.js
@@ -0,0 +1,119 @@
+conts calc
+
+
+
+function calculatorModule (){
+
+  let memory = 0;
+  let total = 0;
+
+
+  /**
+   * sets the `total` to the number passed in
+   * @param  { Number } x
+   * @return { Number }    current total
+   */
+  function load(x){
+    validation(x);
+    total = x;
+    return total;
+  }
+
+
+
+  /**
+   * Return the value of `total`
+   * @return { Number }
+   */
+  function getTotal(){
+    return total;
+  }
+
+
+  /**
+   * Sums the value passed in with `total`
+   * @param { Number } x
+   */
+   function add(x){
+    validation(x);
+    total += x;
+   }
+
+
+  /**
+   * Subtracts the value passed in from `total`
+   * @param  { Number } x
+   */
+  function subtract(x){
+    validation(x);
+    total -= x;
+  }
+
+
+  /**
+   * Multiplies the value by `total`
+   * @param  { Number } x
+   */
+  function multiply(x){
+    validation(x);
+    total = total*x;
+  }
+
+
+
+  /**
+   * Divides the value passing in by `total`
+   * @param  { Number } x
+   */
+
+  function divide(x){
+    validation(x);
+    total = total/x;
+  }
+
+  /**
+   * Return the value stored at `memory`
+   * @return { Number }
+   */
+  function recallMemory(){
+    return memory;
+  }
+
+  /**
+   * Stores the value of `total` to `memory`
+   */
+
+   function saveMemory(){
+    memory = total;
+   }
+
+
+  /**
+   * Clear the value stored at `memory`
+   */
+   function clearMemory(){
+    memory = 0;
+   }
+
+  /**
+   * Validation
+   */
+   function validation(x){
+    if(typeof x !== "number"){
+      throw new Error("Error!!!");
+    }
+   }
+
+   return {
+
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory
+   };
+}
